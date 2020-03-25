c1cf0ec883afb057591df0b4554d30190eb8891a
Michael Böhme <boehme.mic@gmail.com>
2020-03-25 19:45:31 +0100

    upgraded crawler for Jena

--- crawler_jena.py
+++ crawler_jena.py
@@ -8,13 +8,23 @@ DATAFILE = os.path.dirname(os.path.realpath(__file__)) + "/cases_jena.dat"
 def getNumber():
     url         = "https://gesundheit.jena.de/de/coronavirus"
     headers     = { 'Pragma': 'no-cache', 'Cache-Control': 'no-cache' }
-    num_pattern = re.compile(r"\<strong\>\s?([0-9]{1,})\s?\<\/strong\>\s?Meldungen")
     
-    r = requests.get(url, headers=headers, allow_redirects=True, timeout=5.0)
-    ps = num_pattern.findall( r.text )
-    if ( len(ps) == 1 ):
-        return ps[0]
-    else:
+    num_pattern_A = re.compile(r"\<strong\>\s?([0-9]{1,})\s?\<\/strong\>\s?Meldungen")
+    num_pattern_G = re.compile(r"([0-9]{1,})\sPerson.*\sgenesen")
+    num_pattern_T = re.compile(r"([0-9]{1,})\sTodesf")
+    
+    try:
+        r = requests.get(url, headers=headers, allow_redirects=True, timeout=5.0)
+        ps1 = num_pattern_A.findall( r.text )
+        ps2 = num_pattern_G.findall( r.text )
+        ps3 = num_pattern_T.findall( r.text )
+    
+        if ( len(ps1) == 1 ) and ( len(ps2) == 1 ) and ( len(ps3) == 1 ):
+            return (int(ps1[0]), int(ps2[0]), int(ps3[0]))
+        
+        return False
+    
+    except:
         return False  
     
 if __name__ == "__main__":
@@ -23,5 +33,5 @@ if __name__ == "__main__":
     
     if n != False:
         f = open(DATAFILE, 'a')
-        f.write("%-16i %-8i\n" % (int(time.time()), int(n))) 
+        f.write("%-16i %-8i %-8i %-8i\n" % (int(time.time()), n[0], n[1], n[2])) 
         f.close()
