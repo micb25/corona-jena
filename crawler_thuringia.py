a7d141e716a33fce6fa517481f94e94c5926c8b0
Michael Böhme <boehme.mic@gmail.com>
2020-03-25 16:58:53 +0100

    fixed crawler for Thuringia

--- crawler_thuringia.py
+++ crawler_thuringia.py
@@ -8,6 +8,10 @@ DATAFILE = os.path.dirname(os.path.realpath(__file__)) + "/cases_thuringia.dat"
 def strToTimestamp(datetimestr):    
     s = datetimestr.replace("Uhr", "").strip()
     
+    # fix for dates, since 25.03.
+    if re.search(",", s) is None:
+        s += ", 0"
+            
     months = {"Januar": "1", "Februar": "2", "März": "3", "April": "4", "Mai": "5", "Juni": "6", "Juli": "7", "August": "8", "September": "9", "Oktober": "10", "November": "11", "Dezember": "12" }    
     for key in months.keys():
         s = s.replace(key, months[key])
