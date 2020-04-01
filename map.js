f32596c4c4ba9a2ee192e0614593f9854e560747
Florian Kleiner <mail@floriankleiner.de>
2020-04-02 01:23:34 +0200

    moved most js functions to js file, cleanup
    Signed-off-by: Florian Kleiner <mail@floriankleiner.de>

---
+++ map.js
@@ -0,0 +1,93 @@
+var regions = {
+    ABG: "Altenburger Land",
+    EIC: "Eichsfeld",
+    EA:  "Eisenach",
+    EF:  "Erfurt",
+    G:   "Gera",
+    GTH: "Gotha",
+    GRZ: "Greiz",
+    HBN: "Hildburghausen",
+    IK:  "Ilm-Kreis",
+    J:   "Jena",
+    KYF: "Kyffhäuserkreis",
+    NDH: "Nordhausen",
+    SHK: "Saale-Holzland-Kreis",
+    SOK: "Saale-Orla-Kreis",
+    SLF: "Saalfeld-Rudolstadt",
+    SM:  "Schmalkalden-Meiningen",
+    SOM: "Sömmerda",
+    SON: "Sonneberg",
+    SHL: "Suhl",
+    UH:  "Unstrut-Hainich-Kreis",
+    WAK: "Wartburgkreis",
+    WE:  "Weimar",
+    AP:  "Weimarer Land"};
+
+var resultArray = {};
+var max = 0;
+var last_region_bg_color = '';
+        
+function js_goto( id ) {
+    id = id.split('_');
+    var link = './index_th_all.html#graph_' + id[1];
+    window.location.href = link;
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function value_to_color(i, max) {
+    if ( i > 0 ) {
+        val = parseInt( 254 - parseInt( 223.0 * parseInt( i ) / max ) );
+        return "#ffff" + val.toString(16);
+    } else {
+        return "#ffffff";
+    }
+}
+
+function hide_region_texts(    ) {
+    for (var key in regions) {
+            document.getElementById('text_'+ key).style.display='none';
+    }
+}
+
+function m_over_region( id ) {
+    last_region_bg_color = document.getElementById( id ).style.fill;
+    document.getElementById( id ).style.fill = 'rgb(113, 210, 69)';
+    id = id.split('_');
+    document.getElementById( 'cases' ).style.display='block';
+    document.getElementById( 'cases_tspan_region' ).innerHTML = regions[ id[1] ];
+    document.getElementById( 'cases_tspan_count' ).innerHTML = resultArray[ regions[ id[1] ] ];
+    cases_tspan_count
+}
+
+function m_out_region( id ) {
+    document.getElementById( id ).style.fill = last_region_bg_color;
+    id = id.split('_');
+    document.getElementById( 'text_'+ id[1] ).style.display='block';
+    document.getElementById( 'cases' ).style.display='none';
+}
+
+function applyResults() {
+    var found = false;
+    for ( var region in resultArray ) {
+        //console.log( region + ':');
+        found = false;
+        for (var key in regions) {
+            if ( region == regions[key] ) {
+                found = true;
+                //console.log( value_to_color( resultArray[region], max ) );
+                document.getElementById('path_'+ key).style.fill = value_to_color( resultArray[region], max );
+            }
+        }
+        if ( found == false ) {
+            console.log(region + ' not found');
+        }
+    }
+}
