c2a7486d77aa68bb05ced25fce82b6cbae5afbf1
Michael Böhme <boehme.mic@gmail.com>
2020-04-01 20:27:52 +0200

    added map with cases per square kilometer

--- map/generate_map_area.py
+++ map/generate_map_area.py
@@ -7,9 +7,9 @@ from datetime import datetime
 
 def value_to_color(i, imax):
     try:
-        # simple gray color gradient
-        val = int( 254 - int( 0x96 * float(i) / float(imax) ) )
-        return "#{:02x}{:02x}{:02x}".format( val, val, val )
+        # simple blue color gradient
+        val = int( 254 - int( 223.0 * float(i) / float(imax) ) )
+        return "#{:02x}{:02x}ff".format( val, val )
     except:
         return "#ffffff"
 
@@ -21,8 +21,8 @@ if __name__ == "__main__":
     DATAFILE = SCRIPTPATH + "/../data/cases_thuringia.dat"
     TEMPLATE = SCRIPTPATH + "/TH.svg.template"
     SVGFILE  = SCRIPTPATH + "/map_th.svg"
-    JPGFILE  = SCRIPTPATH + "/../map_th_dec.jpg"
-    PNGFILET = SCRIPTPATH + "/../map_th_dec.tmp.png"
+    JPGFILE  = SCRIPTPATH + "/../map_th_rel_area.jpg"
+    PNGFILET = SCRIPTPATH + "/../map_th_rel_area.tmp.png"
     
     # list of placeholders for the colors in the SVG template
     replace_array  = {
@@ -51,6 +51,34 @@ if __name__ == "__main__":
         "Weimarer Land": "%FC_AP%"
     }
     
+    # area size per city/county; values taken from:
+    # https://statistik.thueringen.de/datenbank/TabAnzeige.asp?tabelle=GG000101%7C%7C
+    area_array  = {
+        "Altenburger Land": 569.40,
+        "Eichsfeld":  943.07,
+        "Eisenach": 104.17,
+        "Erfurt":  269.91,
+        "Gera": 152.18,
+        "Gotha":  936.08,
+        "Greiz": 845.98,
+        "Hildburghausen": 938.42,
+        "Ilm-Kreis":  843.71,
+        "Jena": 114.77,
+        "Kyffhäuserkreis": 1037.91,
+        "Nordhausen": 713.90,
+        "Saale-Holzland-Kreis": 815.24,
+        "Saale-Orla-Kreis": 1151.30,
+        "Saalfeld-Rudolstadt":  1036.03,
+        "Schmalkalden-Meiningen": 1210.73,
+        "Sömmerda": 806.86,
+        "Sonneberg": 433.61,
+        "Suhl": 103.03,
+        "Unstrut-Hainich-Kreis":  979.69,
+        "Wartburgkreis":  1307.44,
+        "Weimar": 84.48,
+        "Weimarer Land": 804.48
+    }
+    
     try:
         
         # read data file
@@ -60,18 +88,16 @@ if __name__ == "__main__":
         # get latest timestamp
         timestamp = int(rawdata[-1].split(",")[0])
         
-        # count deceased cases
-        sum_cases = 0
+        # count total cases and assign cases
         max_cases = 0
         area_data = {}
         for l in rawdata:
             ds = l.split(",")
             if ( len(ds) == 8 ):
                 if ( int(ds[0]) == timestamp ):
-                    area_data[ds[1]] = int(ds[6])
-                    sum_cases += int(ds[6])
-                    if ( int(ds[6]) > max_cases ):
-                        max_cases = int(ds[6])
+                    area_data[ds[1]] = float(ds[3]) / float(area_array[ds[1]]) 
+                    if ( area_data[ds[1]] > max_cases ):
+                        max_cases = area_data[ds[1]]
 
         # read SVG template
         with open(TEMPLATE, "r") as df:
@@ -82,11 +108,10 @@ if __name__ == "__main__":
             svgdata = svgdata.replace(r, area_color)
 
         # change labels
-        svgdata = svgdata.replace("%TITLE%", "Verstorbene nach Landkreis/Stadt")
-        svgdata = svgdata.replace("%MIN_VAL%", "0 Verstorbene")
-        svgdata = svgdata.replace("%MID_VAL%", "%i Verstorbene" % (int(max_cases/2)))
-        svgdata = svgdata.replace("%MID_VAL%", "")
-        svgdata = svgdata.replace("%MAX_VAL%", "%i Verstorbene" % (max_cases))
+        svgdata = svgdata.replace("%TITLE%", "relative Fallzahlen pro Quadratkilometer")
+        svgdata = svgdata.replace("%MIN_VAL%", "%.2f Fälle / km²" % (0))
+        svgdata = svgdata.replace("%MID_VAL%", "%.2f Fälle / km²" % (float(max_cases/2)))
+        svgdata = svgdata.replace("%MAX_VAL%", "%.2f Fälle / km²" % (float(max_cases)))
         now = datetime.fromtimestamp(timestamp)
         svgdata = svgdata.replace("%DATE%", now.strftime("letzte Aktualisierung: %d.%m.%Y"))
             
@@ -97,7 +122,7 @@ if __name__ == "__main__":
         
         # create png
         os.system( "convert -resize 800x628 -background '#f2f2f2' -alpha remove -alpha off {} {}".format(SVGFILE, PNGFILET) )
-        os.system( "convert {} gradient_bw.png -gravity northwest -geometry +552+95 -composite -quality 70 -colorspace Gray {}".format(PNGFILET, JPGFILE) )
+        os.system( "convert {} gradient.png -gravity northwest -geometry +552+95 -composite -quality 70 {}".format(PNGFILET, JPGFILE) )
         os.system( "rm -f {}".format(PNGFILET) )
         
     except:
