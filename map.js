1cef5d978c316bc005223524133cd7c00692f247
Florian Kleiner <mail@floriankleiner.de>
2020-04-02 02:10:25 +0200

    added switching functionality and outsourced more js & css
    Signed-off-by: Florian Kleiner <mail@floriankleiner.de>

--- map.js
+++ map.js
@@ -23,7 +23,15 @@ var regions = {
     WE:  "Weimar",
     AP:  "Weimarer Land"};
 
+var types = { diff :  'Entwicklung der Fallzahlen zum Vortag', 
+    cases : 'Fallzahlen',  
+    hosp :  'Stationäre Fälle',  
+    serv :  'Schwere Fallverläufe',  
+    death : 'Verstorbene'};
+
 var resultArray = {};
+var fullResultArray = {};
+var maxArray = {};
 var max = 0;
 var last_region_bg_color = '';
         
@@ -90,4 +98,74 @@ function applyResults() {
             console.log(region + ' not found');
         }
     }
+}
+
+function changeViewTo( id ) {
+  for (var region in fullResultArray) {
+    resultArray[ region ] = fullResultArray[ region ][ id ];
+    max = maxArray[ id ];
+  }
+  // set legend upper limit
+  document.getElementById('upperCount').innerHTML = max;
+  document.getElementById( 'mapHeadline' ).innerHTML = types[ id ] + ' in Thüringen';
+  applyResults();
+}
+
+window.onload = function () {
+    //document.getElementById('svgTitleText').innerHTML="Fälle in Thüringen";
+    // init correct colors
+    document.getElementById('upperLimitColor').setAttribute("stop-color", value_to_color(100, 100) );
+    document.getElementById('lowerLimitColor').setAttribute("stop-color", value_to_color(100, 100) );
+    // hide case count legend
+    document.getElementById( 'cases' ).style.display='none';
+    for (var region in regions) {
+      element = document.getElementById('path_'+ region)
+      element.onmouseover = (function(ev){ m_over_region( ev.target.id ) });
+      element.onmouseout = (function(ev){ m_out_region( ev.target.id ) });
+    
+      element.onclick = (function(ev) { js_goto( ev.target.id ) } );
+    }
+
+    function getText(){
+        // read text from URL location
+        var request = new XMLHttpRequest();
+        var lastTimeStamp = 0;
+        request.open('GET', './data/cases_thuringia.dat', true);
+        request.send(null);
+        request.onreadystatechange = function () {
+          if (request.readyState === 4 && request.status === 200) {
+            data = request.responseText;
+            lines = data.split("\n");
+
+            lines.forEach(element => {
+              if ( element.length > 0 ) {
+                var arr = element.split( "," );
+
+                if ( lastTimeStamp < parseInt( arr[0] ) ) {
+                  lastTimeStamp = parseInt( arr[0] );
+                  maxArray = {  
+                    diff :  parseInt( arr[2] ), 
+                    cases : parseInt( arr[3] ),  
+                    hosp :  parseInt( arr[4] ),  
+                    serv :  parseInt( arr[5] ),  
+                    death : parseInt( arr[6] )}
+                }
+                fullResultArray[ arr[1] ] = {  
+                    diff :  parseInt( arr[2] ), 
+                    cases : parseInt( arr[3] ),  
+                    hosp :  parseInt( arr[4] ),  
+                    serv :  parseInt( arr[5] ),  
+                    death : parseInt( arr[6] )}
+                if ( maxArray['diff'] < fullResultArray[ arr[1] ]['diff'] ) maxArray['diff'] = fullResultArray[ arr[1] ]['diff'];
+                if ( maxArray['cases'] < fullResultArray[ arr[1] ]['cases'] ) maxArray['cases'] = fullResultArray[ arr[1] ]['cases'];
+                if ( maxArray['hosp'] < fullResultArray[ arr[1] ]['hosp'] ) maxArray['hosp'] = fullResultArray[ arr[1] ]['hosp'];
+                if ( maxArray['serv'] < fullResultArray[ arr[1] ]['serv'] ) maxArray['serv'] = fullResultArray[ arr[1] ]['serv'];
+                if ( maxArray['death'] < fullResultArray[ arr[1] ]['death'] ) maxArray['death'] = fullResultArray[ arr[1] ]['death'];
+              }
+            });
+            changeViewTo('cases');
+          }
+      }
+    }
+    getText();
 }
