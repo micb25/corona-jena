e231a00298c7208721ecd668812799612fa96e8c
Florian Kleiner <mail@floriankleiner.de>
2020-04-03 09:31:36 +0200

    code cleanup, mouseover bug on reload, preparation for JSON data set
    Signed-off-by: Florian Kleiner <mail@floriankleiner.de>

--- map.js
+++ map.js
@@ -23,24 +23,30 @@ var regions = {
 	WE:  "Weimar",
 	AP:  "Weimarer Land"};
 
-var types = { diff :  'Entwicklung der Fallzahlen', 
-	cases : 'Fallzahlen',  
-	hosp :  'Stationäre Fälle',  
-	serv :  'Schwere Fallverläufe',  
-	death : 'Verstorbene'};
-	
-var colorByType = { diff :  '#A000FFFF', 
-	cases : '#0000d3',  
-	hosp :  '#FFAD00',  
-	serv :  '#D30000',  
-	death : '#333333'};
+// map types and colors
+// { var_key : { de: 'xxx', en: 'xxx', color: '#000000' } }
+var types = {
+	cases : {
+		de: 'Fallzahlen',
+		color: '#0000D3' },
+	diff : {
+		de: 'Entwicklung der Fallzahlen',
+		color: '#A000FF' },
+	hosp : {
+		de: 'Stationäre Fälle',
+		color: '#FFAD00' },
+	serv : {
+		de: 'Schwere Fallverläufe',
+		color: '#D30000' },
+	death : {
+		de: 'Verstorbene', 
+		color: '#333333' }
+};
 
 var resultArray = {};
 var fullResultArray = {};
 var maxArray = {};
-var max = 0;
-var last_region_bg_color = '';
-var color = '';
+var langKey = 'de';
 		
 function js_goto( id ) {
 	id = id.split('_');
@@ -64,58 +70,37 @@ function value_to_color(i, max, maxColor) {
 		var r = Math.ceil(parseInt(maxColor.substring(1,3), 16) * ratio + parseInt(white.substring(1,3), 16) * (1-ratio));
 		var g = Math.ceil(parseInt(maxColor.substring(3,5), 16) * ratio + parseInt(white.substring(3,5), 16) * (1-ratio));
 		var b = Math.ceil(parseInt(maxColor.substring(5,7), 16) * ratio + parseInt(white.substring(5,7), 16) * (1-ratio));
-		resultColor = '#' + hex(r) + hex(g) + hex(b);
-		
-		//console.log(i + '/' +max + ': h' +hex(r) + ', d' + r);
-		
+		resultColor = '#' + hex(r) + hex(g) + hex(b);		
 	} else {
 		resultColor = white;
 	}
-	//console.log( resultColor );
 	return resultColor;
 }
 
 function hide_region_texts(	) {
-	for (var key in regions) {
-			document.getElementById('text_'+ key).style.display='none';
+	for ( var region in regions ) {
+		document.getElementById( 'text_' + region ).style.display = 'none';
 	}
 }
 
 function m_over_region( id ) {
-	console.log(id);
-	id = id.split('_');
-	last_region_bg_color = document.getElementById( 'path_' + id[1] ).style.fill;
-	document.getElementById( 'path_' + id[1] ).style.fill = 'rgb(113, 210, 69)';
-	document.getElementById( 'cases' ).style.display='block';
+	id = id.split( '_' );
+	document.getElementById( 'path_' + id[1] ).style.fill = '#8BC34A';
+	document.getElementById( 'cases' ).style.display = 'block';
 	document.getElementById( 'cases_tspan_region' ).innerHTML = regions[ id[1] ];
-	document.getElementById( 'cases_tspan_count' ).innerHTML = resultArray[ regions[ id[1] ] ];
+	document.getElementById( 'cases_tspan_count' ).innerHTML = resultArray[ id[1] ]['value'];
 }
 
 function m_out_region( id ) {
 	id = id.split('_');
-	document.getElementById( 'path_' + id[1] ).style.fill = last_region_bg_color;
-	document.getElementById( 'cases' ).style.display='none';
+	document.getElementById( 'path_' + id[1] ).style.fill = resultArray[ id[1] ]['color'];
+	document.getElementById( 'cases' ).style.display = 'none';
 }
 
-function applyResults() {
+function applyResults( resultArray ) {
 	var found = false;
 	
-	// init color legend
-	document.getElementById('upperLimitColor').setAttribute("stop-color", value_to_color(1,1, color) );
 	
-	for ( var region in resultArray ) {
-		//console.log( region + ':');
-		found = false;
-		for (var key in regions) {
-			if ( region == regions[key] ) {
-				found = true;
-				document.getElementById('path_'+ key).style.fill = value_to_color( resultArray[region], max, color );
-			}
-		}
-		if ( found == false ) {
-			console.log(region + ' not found');
-		}
-	}
 }
 
 function changeViewTo( id ) {
@@ -124,95 +109,115 @@ function changeViewTo( id ) {
 	}
 	document.getElementById( id ).style.fontWeight = "bold";
 	id = id.split('_');
-	for (var region in fullResultArray) {
-		resultArray[ region ] = fullResultArray[ region ][ id[1] ];
-		max = maxArray[ id[1] ];
+	for (var regionKey in fullResultArray) {
+		// populate result array to be able to read out the data on mouse over
+		resultArray[ regionKey ] = {};
+		resultArray[ regionKey ]['value'] = fullResultArray[ regionKey ][ id[1] ];
+		resultArray[ regionKey ]['color'] = value_to_color( resultArray[ regionKey ]['value'], maxArray[ id[1] ], types[ id[1] ][ 'color' ] );
+		// apply color to map
+		document.getElementById( 'path_'+ regionKey ).style.fill = resultArray[ regionKey ]['color'];
 	}
 	// set legend upper limit
-	document.getElementById( 'upperCount' ).innerHTML = max;
-	document.getElementById( 'mapHeadline' ).innerHTML = types[ id[1] ] + ' in Thüringen';
-	color = colorByType[ id[1] ];
-	applyResults();
+	document.getElementById( 'upperCount' ).innerHTML = maxArray[ id[1] ];
+	document.getElementById( 'mapHeadline' ).innerHTML = types[ id[1] ][ langKey ] + ' in Thüringen';
+
+	// init color legend
+	document.getElementById('upperLimitColor').setAttribute("stop-color", value_to_color( 1, 1, types[ id[1] ][ 'color' ] ) );
 }
 
 function generateMenu() {
 	del = '';
 	menu = '';
-	for (var key in types) {
-		menu = menu + del + '<span id="selector_' + key +'">' + types[ key ] + '</span>';
+	for ( var key in types ) {
+		menu = menu + del + '<span id="selector_' + key +'">' + types[ key ][ langKey ] + '</span>';
 		del = '&nbsp;|&nbsp;';
 	}
 	
-	document.getElementById('selectorLinks').innerHTML = menu;
-	for (var key in types) {
-		document.getElementById( 'selector_' + key ).onclick = (function(ev) { changeViewTo( ev.target.id ) } );
+	document.getElementById( 'selectorLinks' ).innerHTML = menu;
+	for ( var key in types ) {
+		document.getElementById( 'selector_' + key ).onclick = (function(e) { changeViewTo( e.target.id ) } );
 	}
 	
 }
 
-function getText(){
+function setDataTimeTo( timestamp ) {
+	var months = ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'];
+	var date = new Date( timestamp * 1000 );
+	var month = months[ date.getMonth() ];
+	var min = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes(); 
+	var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate(); 
+	document.getElementById( 'tspan_timestamp' ).innerHTML = 'Stand: ' + day + '. ' +month + ' ' + date.getFullYear() + ', ' + date.getHours() + ':' + min + ' Uhr';
+}
+
+function getData(){
 	// read text from URL location
 	var request = new XMLHttpRequest();
 	var lastTimeStamp = 0;
 	request.open('GET', './data/cases_thuringia.dat', true);
 	request.send(null);
 	request.onreadystatechange = function () {
-	  if (request.readyState === 4 && request.status === 200) {
-		data = request.responseText;
-		lines = data.split("\n");
+	  	if (request.readyState === 4 && request.status === 200) {
+			data = request.responseText;
+			lines = data.split("\n");
 
-		lines.forEach(element => {
-		  if ( element.length > 0 ) {
-			var arr = element.split( "," );
+			lines.forEach( element => {
+				if ( element.length > 0 ) {
+					var arr = element.split( "," );
 
-			if ( lastTimeStamp < parseInt( arr[0] ) ) {
-			  lastTimeStamp = parseInt( arr[0] );
-			  maxArray = {  
-				diff :  parseInt( arr[2] ), 
-				cases : parseInt( arr[3] ),  
-				hosp :  parseInt( arr[4] ),  
-				serv :  parseInt( arr[5] ),  
-				death : parseInt( arr[6] )}
-			}
-			var months = ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'];
-			var date = new Date(lastTimeStamp * 1000);
-			var month = months[date.getMonth()];
-			var min = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes(); 
-			var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate(); 
-			document.getElementById( 'tspan_timestamp' ).innerHTML = 'Stand: ' + day + '.' +month + ' ' + date.getFullYear() + ', ' + date.getHours() + ':' + min + ' Uhr';
-			
-			fullResultArray[ arr[1] ] = {  
-				diff :  parseInt( arr[2] ), 
-				cases : parseInt( arr[3] ),  
-				hosp :  parseInt( arr[4] ),  
-				serv :  parseInt( arr[5] ),  
-				death : parseInt( arr[6] )}
-			for (var type in types) {
-				if ( maxArray[ type ] < fullResultArray[ arr[1] ][ type ] ) maxArray[ type ] = fullResultArray[ arr[1] ][ type ];
-			}
-		  }
-		});
-		generateMenu();
-		changeViewTo('selector_cases');
-	  }
+					// set latest timestamp and reset max value array
+					if ( lastTimeStamp < parseInt( arr[0] ) ) {
+						lastTimeStamp = parseInt( arr[0] );
+						
+						maxArray = {  
+							diff :  parseInt( arr[2] ), 
+							cases : parseInt( arr[3] ),  
+							hosp :  parseInt( arr[4] ),  
+							serv :  parseInt( arr[5] ),  
+							death : parseInt( arr[6] )
+						}
+					}
+					
+					// find the correct key to the region name
+					var regionKey = '';
+					for (var key in regions) {
+						if ( regions[ key ] == arr[1] ) regionKey = key;
+					}
+
+					// fill the result array
+					fullResultArray[ regionKey ] = {
+						diff :  parseInt( arr[2] ), 
+						cases : parseInt( arr[3] ),  
+						hosp :  parseInt( arr[4] ),  
+						serv :  parseInt( arr[5] ),  
+						death : parseInt( arr[6] )
+					}
+
+					// get max value for evry map
+					for ( var key in types ) {
+						if ( maxArray[ key ] < fullResultArray[ regionKey ][ key ] ) maxArray[ key ] = fullResultArray[ regionKey ][ key ];
+					}
+				}
+			});
+			setDataTimeTo( lastTimeStamp );
+			generateMenu();
+			changeViewTo( 'selector_' + Object.keys(types)[0] );
+		}
 	}
-	
 }
+
 window.onload = function () {
 	// hide case count legend
-	document.getElementById( 'cases' ).style.display='none';
-	for (var region in regions) {
-	  element = document.getElementById('path_'+ region)
-	  element.onmouseover = (function(ev){ m_over_region( ev.target.id ) });
-	  element.onmouseout = (function(ev){ m_out_region( ev.target.id ) });
-	  element.onclick = (function(ev) { js_goto( ev.target.id ) } );
-
-	  element = document.getElementById('text_'+ region)
-	  element.onmouseover = (function(ev){ m_over_region( ev.target.id ) });
-	  element.onmouseout = (function(ev){ m_out_region( ev.target.id ) });
-	  element.onclick = (function(ev) { js_goto( ev.target.id ) } );
+	document.getElementById( 'cases' ).style.display = 'none';
+    var elementTypes = ['path_', 'text_'];
+	for ( var region in regions ) {
+		elementTypes.forEach( elementType => {
+			obj = document.getElementById( elementType + region )
+			obj.onmouseover = ( function(e){ m_over_region( e.target.id ) } );
+			obj.onmouseout = ( function(e){ m_out_region( e.target.id ) } );
+			obj.onclick = ( function(e) { js_goto( e.target.id ) } );
+		});
 	}
 
-	getText();
-	document.getElementById( 'thuringiaMap' ).style.display='block';
+	getData();
+	document.getElementById( 'thuringiaMap' ).style.display = 'block';
 }
