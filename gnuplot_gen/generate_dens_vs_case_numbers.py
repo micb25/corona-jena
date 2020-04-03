2a7b474031336fa418e23589641c981f6eaf0cd3
Michael Böhme <boehme.mic@gmail.com>
2020-04-03 21:25:54 +0200

    added plot with density vs cases

---
+++ gnuplot_gen/generate_dens_vs_case_numbers.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+if __name__ == "__main__":
+    
+    SCRIPTPATH = os.path.dirname(os.path.realpath(__file__))
+    
+    DATAFILE = SCRIPTPATH + "/../data/cases_thuringia.dat"
+    DATFILE  = SCRIPTPATH + "/th_dens_vs_cases.dat"
+    GPFILE   = SCRIPTPATH + "/plotT1_pop_dens.gnuplot"
+    
+    replace_array  = {
+        "Altenburger Land": "ABG",
+        "Eichsfeld": "EIC",
+        "Eisenach": "EA",
+        "Erfurt": "EF",
+        "Gera": "G",
+        "Gotha": "GTH",
+        "Greiz": "GRZ",
+        "Hildburghausen": "HBN",
+        "Ilm-Kreis": "IK",
+        "Jena": "J",
+        "Kyffhäuserkreis": "KYF",
+        "Nordhausen": "NDH",
+        "Saale-Holzland-Kreis": "SHK",
+        "Saale-Orla-Kreis": "SOK",
+        "Saalfeld-Rudolstadt": "SLF",
+        "Schmalkalden-Meiningen": "SM",
+        "Sömmerda": "SÖM",
+        "Sonneberg": "SON",
+        "Suhl": "SHL",
+        "Unstrut-Hainich-Kreis": "UH",
+        "Wartburgkreis": "WAK",
+        "Weimar": "WE",
+        "Weimarer Land": "AP"
+    }
+   
+    # density per city/county; values taken from:
+    # https://statistik.thueringen.de/datenbank/TabAnzeige.asp?tabelle=GG000101%7C%7C
+    area_array  = {
+        "Altenburger Land": 90118.0 / 569.40,
+        "Eichsfeld":  100380.0 / 943.07,
+        "Eisenach": 42370.0 / 104.17,
+        "Erfurt":  213699.0 / 269.91,
+        "Gera": 94152.0 / 152.18,
+        "Gotha":  135452.0 / 936.08,
+        "Greiz": 98159.0 / 845.98,
+        "Hildburghausen": 63553.0 / 938.42,
+        "Ilm-Kreis":  108742.0 / 843.71,
+        "Jena": 111407.0 / 114.77,
+        "Kyffhäuserkreis": 75009.0 / 1037.91,
+        "Nordhausen": 83822.0 / 713.90,
+        "Saale-Holzland-Kreis": 83051.0 / 815.24,
+        "Saale-Orla-Kreis": 80868.0 / 1151.30,
+        "Saalfeld-Rudolstadt":  106356.0 / 1036.03,
+        "Schmalkalden-Meiningen": 122347.0 / 1210.73,
+        "Sömmerda": 69655.0 / 806.86,
+        "Sonneberg": 56196.0 / 433.61,
+        "Suhl": 34835.0 / 103.03,
+        "Unstrut-Hainich-Kreis":  102912.0 / 979.69,
+        "Wartburgkreis": 123025.0 / 1307.44,
+        "Weimar": 65090.0 / 84.48,
+        "Weimarer Land": 81947.0 / 804.48
+    }
+    
+    cities = ["Eisenach", "Erfurt", "Gera", "Jena", "Suhl", "Weimar"]
+        
+    try:
+        
+        # read data file
+        with open(DATAFILE, "r") as df:
+            rawdata = df.read().splitlines()
+
+        # get latest timestamp
+        timestamp = int(rawdata[-1].split(",")[0])
+        
+        # generate and write data
+        with open(DATFILE, "w") as dat:
+            for l in rawdata:
+                ds = l.split(",")
+                if ( len(ds) == 8 ):
+                    if ( int(ds[0]) == timestamp ):
+                        dat.write("%s,%s,%i,%.3f,%i\n" % (ds[1], replace_array[ds[1]], 1 if ds[1] in cities else 0, float(area_array[ds[1]]), float(ds[3])) )
+            
+            dat.close()            
+
+        # create diagram
+        os.system( "gnuplot {} > /dev/null".format( GPFILE ) )
+        
+    except:
+        print("Error occured!")
---
+++ gnuplot_gen/plotT1_pop_dens.gnuplot
@@ -0,0 +1,79 @@
+set terminal pngcairo enhanced background rgb "#f8f8f8" truecolor font "Linux Libertine O,16" size 800, 600 dl 2.0 
+set encoding utf8
+set minussign
+
+set fit quiet logfile '/dev/null'
+set fit errorvariables
+
+# margins
+set lmargin 12.25
+set rmargin 2.80
+set tmargin 0.75
+set bmargin 3.75
+
+# colors and plot style
+set style line  1 lc rgb '#0000FF' lt 1 lw 1 pt 7 ps 1.50 dt "."
+set style line  2 lc rgb '#800000' lt 1 lw 1 pt 7 ps 1.50 dt "."
+set style line  3 lc rgb '#000000' lt 1 lw 1 pt 7 ps 1.50
+set style line  4 lc rgb '#005000' lt 1 lw 1 pt 7 ps 1.50 dt "."
+set style line  5 lc rgb '#000000' lt 1 lw 1 pt 7 ps 1.50 dt "."
+set style line  6 lc rgb '#000080' lt 1 lw 1 pt 7 ps 1.50 dt "."
+set style line  10 lc rgb '#000000' lw 1 lt 1 dt "  .  "
+set style line  11 lc rgb '#aaaaaa' lw 1 lt 1 dt "  .  "
+set style line  12 lc rgb '#FF0000' lw 2
+set style line  21 dt 3
+
+# grid
+set grid xtics ls 21 lc rgb '#aaaaaa'
+set grid ytics ls 21 lc rgb '#aaaaaa'
+
+# misc
+set samples 30
+set style increment default
+ set style fill transparent solid 0.20 border
+
+# axes
+set xtics out nomirror scale 1.2
+set mxtics 2
+
+set format y '%6.0f'
+set ytics out nomirror scale 1.2
+set mytics 2
+
+set key opaque
+set border back
+
+set object 1 rectangle from screen -0.1,-0.1 to screen 1.1,1.1 fc rgb "#f2f2f2" behind
+
+# filter negative values
+filter_neg(x)=(x>=0)?(x):(1/0)
+
+# latest update
+update_str = "{/*0.75 (Stand: " . system("date +%d.%m.,\\ %H\\:%M") . " Uhr)}"
+
+set output '../plotT1_pop_dens.png'
+
+set xrange [ 0 : 1000 ]
+
+# stats for y
+stats "<awk -F, '{ print $5 }' ./th_dens_vs_cases.dat" using 1 nooutput
+set yrange [ 0 : int(4/3.0*STATS_max) ]
+
+# x-axis setup
+set xlabel "Einwohnerdichte (EW/km²)"
+
+# y-axis setup
+set ylabel sprintf("Zahl bestätigter Coronavirus-Fälle %s", update_str)
+
+# key
+set key at graph 0.02, 0.98 left top invert spacing 1.2 box ls 3
+# unset key
+
+# data
+plot  \
+  1/0 notitle, \
+  "<awk -F, '{if ($3 == 0) print $4, $5}' ./th_dens_vs_cases.dat" u 1:2 w p ls 2 title "Landkreise", \
+  "<awk -F, '{if ( ($3 == 0) && ( $4 > 70 ) ) print $4, $5, $2}' ./th_dens_vs_cases.dat" u 1:2:($3) with labels point pt 7 ps 0 center offset char -0.3, 0.8 tc ls 2 notitle, \
+  "<awk -F, '{if ($3 == 1) print $4, $5}' ./th_dens_vs_cases.dat" u 1:2 w p ls 1 title "kreisfreie Städte", \
+  "<awk -F, '{if ($3 == 1) print $4, $5, $2}' ./th_dens_vs_cases.dat" u 1:2:($3) with labels point pt 7 ps 0 center offset char -0.3, 0.8 tc ls 1 notitle
+  
