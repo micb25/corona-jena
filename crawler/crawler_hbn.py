364e65678f2003d756d2cf428db9effdc97dd643
Michael Böhme <boehme.mic@gmail.com>
2020-09-23 18:33:00 +0200

    fixed scraper for HBN

--- crawler/crawler_hbn.py
+++ crawler/crawler_hbn.py
@@ -2,24 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import datetime, requests, re, os
+from num2words import num2words
 
+number_dict = {}
+number_dict["kein"] = 0
+number_dict["keine"] = 0
+number_dict["keiner"] = 0
+number_dict["eine"] = 1
+number_dict["einer"] = 1
 
+for i in range(1, 501):
+    number_dict[ num2words(i, lang='de') ] = i
+    
 def germanWordToInt(w):   
     if re.match("^[0-9]{1,}$", w) is not None:
         return int(w)
-    else:        
-        s = w.lower()        
-        number_dict = {
-                    "eine": 1, "einer": 1, "ein": 1, "zwei": 2, "drei": 3, "vier": 4, "fünf": 5, "sechs": 6, "sieben": 7, "acht": 8, "neun": 9, "zehn": 10,
-                    "elf": 11, "zwölf": 12, "dreizehn": 13, "vierzehn": 14, "fünfzehn": 15, "sechzehn": 16, "siebzehn": 17, "achtzehn": 18, "neunzehn": 19, "zwanzig": 20,
-                    "einundzwanzig": 21, "zweiundzwanzig": 22, "dreiundzwanzig": 23, "vierundzwanzig": 24, "fünfundzwanzig": 25, "sechsundzwanzig": 26, "siebenundzwanzig": 27, "achtundzwanzig": 28, "neunundzwanzig": 29, "dreißig": 30,
-                    "einunddreißig": 31, "zweiunddreißig": 32, "dreiunddreißig": 33, "vierunddreißig": 34, "fünfunddreißig": 35, "sechsunddreißig": 36, "siebenunddreißig": 37, "achtunddreißig": 38, "neununddreißig": 39, "vierzig": 40,
-                    "einundvierzig": 41, "zweiundvierzig": 42, "dreiundvierzig": 43, "vierundvierzig": 44, "fünfundvierzig": 45, "sechsundvierzig": 46, "siebenundvierzig": 47, "achtundvierzig": 48, "neunundvierzig": 49, "fünfzig": 50,
-                    "einundfünfzig": 51, "zweiundfünfzig": 52, "dreiundfünfzig": 53, "vierundfünfzig": 54, "fünfundfünfzig": 55, "sechsundfünfzig": 56, "siebenundfünzig": 57, "achtundfünfzig": 58, "neunundfünzig": 59, "sechzig": 60,
-                    "einundsechzig": 61, "zweiundsechzig": 62, "dreiundsechzig": 63, "vierundsechzig": 64, "fünfundsechzig": 65, "sechsundsechzig": 66, "siebenundsechzig": 67, "achtundsechzig": 68, "neunundsechzig": 69, "siebzig": 70
-                }        
+    else:  
         for n in number_dict:
-            if ( s == n ):
+            if ( w.lower() == n ):
                 return number_dict[n]
         return False
     
@@ -37,7 +37,6 @@ def getHBNNumbers(url):
     headers = { 'Pragma': 'no-cache', 'Cache-Control': 'no-cache' }
     
     pattern_Subpage = re.compile(r"<small class=\"date\">([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4,4})</small>\s*?<h4 class=\"liste-titel\"><a href=\"(.*?)\">.*?Aktuelle\sFallzahlen.*?</a></h4>")
-    pattern_PM = re.compile(r"<h2 class=\"toggler-title\">([0-9][^<]*Fallzahlen[^<]*)<\/h2>(.*?)</div>", re.DOTALL)
     pattern_date = re.compile(r"([0-9]{1,})\.([0-9]{1,}).([0-9]{2,4}),\s?([0-9]{1,})[\.:]([0-9]{1,})")    
     
     num_pattern_T = re.compile(r"\s([^\s]*)\s(?:positiv\sgetestete\sPersonen|Personen\spositiv)")
@@ -137,7 +136,7 @@ if __name__ == "__main__":
         # deceased number is not always included in new reports
         if value_changed:
             num_latest[3] = max(num_latest[3], int(last_values[2]))
-                    
+            
         if value_changed:
             # write new csv data
             f = open(DATAFILE, 'a')
