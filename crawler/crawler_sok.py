bdda3f082ab30e6e52556c2e7982be14668123a6
Michael Böhme <boehme.mic@gmail.com>
2020-09-23 19:09:45 +0200

    fixed scraper for SOK

--- crawler/crawler_sok.py
+++ crawler/crawler_sok.py
@@ -8,22 +8,24 @@ def getSOKNumbers(url):
     headers = { 'Pragma': 'no-cache', 'Cache-Control': 'no-cache' }
         
     num_pattern_T = re.compile(r"Infektionen\s(?:gesamt|insgesamt):\s?(?:<br>)?([0-9]{1,})")
+    num_pattern_A = re.compile(r"[aA]ktive Infektionen:\s?([0-9]{1,})")
     num_pattern_D = re.compile(r"Verstorbene: ([0-9]{1,})")
     
     try:
         r = requests.get(url, headers=headers, allow_redirects=True, timeout=5.0)
-        s = r.text
+        s = r.text.replace("<br>", "").replace("<strong>", "")
         
         ps1 = num_pattern_T.findall( s )
+        ps2 = num_pattern_A.findall( s )
         ps3 = num_pattern_D.findall( s )
         
         num_t = int(ps1[0]) if (len(ps1) >= 1) else -1
-        num_r = -1
         num_d = int(ps3[0]) if (len(ps3) >= 1) else -1
+        num_r = num_t - num_d - int(ps2[0]) if num_t > -1 and num_d > -1 and (len(ps2) >= 1) else -1
         num_h = -1
         num_s = -1
         
-        return (num_t, num_r, num_d, num_h, num_s)
+        return [num_t, num_r, num_d, num_h, num_s]
     
     except:
         return False  
@@ -32,7 +34,7 @@ def getSOKNumbers(url):
 if __name__ == "__main__":
     
     DATAFILE = os.path.dirname(os.path.realpath(__file__)) + "/../data/cases_sok.csv"
-    URL = 'https://www.saale-orla-kreis.de/sok/'
+    URL = 'https://www.saale-orla-kreis.de/de/corona/aktuelle-mitteilungen-und-informationen-zur-corona-situation-im-saale-orla-kreis.html'
     
     num_latest = getSOKNumbers(URL)
     
