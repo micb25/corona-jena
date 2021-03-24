bdf2594cd9a0613ed3f62e5a1fae1a77d46b15f5
Michael Böhme <boehme.mic@gmail.com>
2021-03-24 03:29:38 +0100

    implemented crawler for vaccination data

---
+++ crawler/crawler_thuringia_vac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests, os
+
+if __name__ == "__main__":
+    
+    DATAFILE = os.path.dirname(os.path.realpath(__file__)) + "/../data/RKI_COVID19_Impfquotenmonitoring.csv"
+    URL = 'https://raw.githubusercontent.com/micb25/RKI_COVID19_DATA/master/Impfquotenmonitoring/RKI_COVID19_Impfquotenmonitoring.csv'
+    
+    # do the request
+    headers = { 'Pragma': 'no-cache', 'Cache-Control': 'no-cache' }
+    r = requests.get(URL, headers=headers, allow_redirects=True, timeout=5.0)
+    
+    if r.ok:            
+        # write new data
+        f = open(DATAFILE, 'a')
+        f.write(r.text)
+        f.close()
+            
