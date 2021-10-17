26385e303941dd3abd894e483d981f811aa85261
Michael Böhme <boehme.mic@gmail.com>
2021-10-18 00:53:59 +0200

    improved hospitalization data

--- crawler/crawler_rki_th_hosp.py
+++ crawler/crawler_rki_th_hosp.py
@@ -21,16 +21,41 @@ def getRKIHospitalizationData():
     df.drop('Datum', axis=1, inplace=True)
     df['Altersgruppe'] = df['Altersgruppe'].astype('category')
 
+    # zero-fill
+    ags = list(df['Altersgruppe'].unique())
+    min_date = df['Timestamp'].min()
+    for i in range(1, 10):
+        for ag in ags:
+            zero_row = { 'Timestamp': min_date - i * 86400, 'Altersgruppe': ag, '7T_Hospitalisierung_Faelle': 0, '7T_Hospitalisierung_Inzidenz': 0.00 }
+            df = df.append(zero_row, ignore_index=True)
+
     # sort rows
     df.sort_values(by=['Timestamp', 'Altersgruppe'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
 
     # sort columns
     cols = df.columns.tolist()
     cols = cols[-1:] + cols[:-1]
     df = df[cols]
 
-    # calculate average cases by day 
-    df['Hospitalisierung_Faelle'] = df['7T_Hospitalisierung_Faelle'] / 7
+    # calculate cases by day
+    df['Hospitalisierung_Faelle'] = df['Timestamp'].apply(lambda r: 0 if (r <= min_date) else -1 )
+    df['Hospitalisierung_Summe'] = 0
+    for ag in ags:
+        df_sub = df[ df.Altersgruppe == ag ]
+        sum_cases = 0
+        for i, r in df_sub.iterrows():
+            if ( r['Timestamp'] >= min_date ):
+                prev_row = df_sub.iloc[df_sub.index.get_loc(i)-1]
+                prev_week = df_sub.iloc[df_sub.index.get_loc(i)-7]
+                hosp_cases = r['7T_Hospitalisierung_Faelle'] - prev_row['7T_Hospitalisierung_Faelle'] + prev_week['Hospitalisierung_Faelle']
+                sum_cases += hosp_cases
+                df_sub.at[i, 'Hospitalisierung_Faelle'] = hosp_cases
+                df.at[i, 'Hospitalisierung_Faelle'] = hosp_cases
+                df.at[i, 'Hospitalisierung_Summe'] = sum_cases
+
+    # drop zero-fill
+    df.drop(df[ df.Timestamp < min_date ].index  , axis=0, inplace=True)
 
     # export data
     df.to_csv('../data/RKI_TH_Hospitalisierung.csv', float_format='%.2f', sep=",", decimal=".", encoding='utf-8', index=False)
